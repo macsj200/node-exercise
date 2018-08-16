5acc03487dc56ddd985d04cd39d7eb8e68ab459f
macsj200 <maxjohansen@berkeley.edu>
2018-08-15 18:58:26 -0600

    res.json

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const swapi = require('./swapi');
 
 const app = express();
 
-app.get('/people', async (req, res) => res.send(await swapi.fetchPeople(req.query.sortBy)));
+app.get('/people', async (req, res) => res.json(await swapi.fetchPeople(req.query.sortBy)));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
